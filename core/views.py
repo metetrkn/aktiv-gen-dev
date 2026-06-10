6b65d9c9376627a81e01e110064457b5a232c215
mete trkn <metetrkn52@gmail.com>
2026-06-10 20:55:59 +0200

    Add Post feat

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # Fetch all posts, ordered by the newest first
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'core/post_list.html', {'posts': posts})
